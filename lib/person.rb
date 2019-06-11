ca02c279abebf8c51816c1f6039f214469ff9b2d
“Patrick <pctennislessons@gmail.com>
2019-06-11 13:14:16 -0600

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+
+    def initalize(name, job)
+        @name = name
+        @job = job
+    end
+
+    def name=(a_name)
+        @name = a_name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(a_job)
+        @job = a_job
+    end
+
+    def job
+        @job
+    end
+end
